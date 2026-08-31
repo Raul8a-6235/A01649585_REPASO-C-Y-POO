0a47e2e0f422478af6396d3cc0f579a361b0b7bd
Raul8a-6235 <A01649585@tec.mx>
2026-08-31 19:31:40 +0000

    R0: compilacion de tres archivos desde la terminal

---
+++ r00-compilacion/Suma.cpp
@@ -0,0 +1,5 @@
+#include "Suma.h"
+
+int suma(int a, int b){
+    return a + b;
+}
---
+++ r00-compilacion/main.cpp
@@ -0,0 +1,8 @@
+#include "Suma.h"
+#include <iostream>
+#include <string>
+
+int main(){
+    std::cout << suma(5, 3)<<std::endl;
+    return 0;
+}
